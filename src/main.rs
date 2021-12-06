395d9ad61948e1df61a51e721cc383d6dc498337
colton <cloftus@princeton.edu>
2021-12-06 11:24:26 -0500

    Graphics working

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,9 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
     println!("It did not crash!");
-    loop {}
+    hello_world_graphics();
+    loop {
+    }
 }
 
 /// This function is called on panic.
@@ -34,4 +36,22 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     hobby_os::test_panic_handler(info)
+}
+
+
+pub fn hello_world_graphics() -> () {
+    use vga::colors::Color16;
+    use vga::writers::{Graphics640x480x16, GraphicsWriter};
+
+    let mode = Graphics640x480x16::new();
+    mode.set_mode();
+    mode.clear_screen(Color16::Black);
+    mode.draw_line((80, 60), (80, 420), Color16::White);
+    mode.draw_line((80, 60), (540, 60), Color16::White);
+    mode.draw_line((80, 420), (540, 420), Color16::White);
+    mode.draw_line((540, 420), (540, 60), Color16::White);
+    mode.draw_line((80, 90), (540, 90), Color16::White);
+    for (offset, character) in "Hello World!".chars().enumerate() {
+        mode.draw_character(270 + offset * 8, 72, character, Color16::White)
+    }
 }

fdb406bc0647ec0907ec4f3e909939044c6a5eee
colton <cloftus@princeton.edu>
2022-05-12 10:22:26 -0400

    allocators

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,10 +1,18 @@
+pub mod bump;
+pub mod linked_list;
+pub mod fixed_size_block;
+
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-
 use linked_list_allocator::LockedHeap;
+use bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(
+    FixedSizeBlockAllocator::new());
+
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -47,4 +55,26 @@ pub fn init_heap(
 #[alloc_error_handler]
 fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
     panic!("allocation error: {:?}", layout)
+}
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
